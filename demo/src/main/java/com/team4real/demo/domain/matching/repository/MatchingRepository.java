f318a0bf44ace36b4edf50c1cc70a8641a68fd60
Seohyun Lee <applemint2003@gmail.com>
2025-07-29 16:02:09 +0900

    feat: 매칭 상태별 조회 api 무한스크롤 방식으로 개발

--- demo/src/main/java/com/team4real/demo/domain/matching/repository/MatchingRepository.java
+++ demo/src/main/java/com/team4real/demo/domain/matching/repository/MatchingRepository.java
@@ -4,11 +4,26 @@ import com.team4real.demo.domain.brand.entity.Brand;
 import com.team4real.demo.domain.creator.entity.Creator;
 import com.team4real.demo.domain.matching.entity.Matching;
 import com.team4real.demo.domain.matching.entity.MatchingStatus;
-import org.springframework.data.domain.Page;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
-    Page<Matching> findByBrandAndStatus(Brand brand, MatchingStatus status, Pageable pageable);
-    Page<Matching> findByCreatorAndStatus(Creator creator, MatchingStatus status, Pageable pageable);
+    @Query("SELECT m FROM Matching m JOIN FETCH m.brand WHERE m.creator = :creator AND m.status = :status AND m.matchingId < :lastId ORDER BY m.matchingId DESC")
+    List<Matching> findWithBrandByCursor(@Param("creator") Creator creator, @Param("status") MatchingStatus status, @Param("lastId") Long lastId, Pageable pageable);
+    @Query("""
+    SELECT m FROM Matching m
+    JOIN FETCH m.creator
+    WHERE m.brand = :brand AND m.status = :status
+    AND (:lastMatchingId IS NULL OR m.matchingId < :lastMatchingId)
+    ORDER BY m.matchingId DESC
+    """)
+    List<Matching> findByBrandAndStatusWithCursor(
+            @Param("brand") Brand brand,
+            @Param("status") MatchingStatus status,
+            @Param("lastMatchingId") Long lastMatchingId,
+            Pageable pageable);
 }
