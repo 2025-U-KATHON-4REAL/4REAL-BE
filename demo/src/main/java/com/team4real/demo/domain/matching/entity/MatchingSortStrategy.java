6718ae2aebf4b534afd805656eb3774841894b38
Seohyun Lee <applemint2003@gmail.com>
2025-08-07 04:52:39 +0900

    refactor: 주석 삭제

--- demo/src/main/java/com/team4real/demo/domain/matching/entity/MatchingSortStrategy.java
+++ demo/src/main/java/com/team4real/demo/domain/matching/entity/MatchingSortStrategy.java
@@ -1,7 +1,5 @@
 package com.team4real.demo.domain.matching.entity;
 
-import org.springframework.data.domain.Sort;
-
 public enum MatchingSortStrategy {
     BEST_MATCH,     // 매칭률순
     POPULARITY,     // 좋아요순
